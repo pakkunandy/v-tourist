2a505aa2dd97a7d9c7dd151a66c6c11c981e545c
Đinh Lê Mạnh Duy <manhduydl@gmail.com>
2015-12-18 20:17:04 +0700

    update detail

--- Source/Client Side/V-Tourist/app/src/main/java/com/group5/controller/DetailActivity.java
+++ Source/Client Side/V-Tourist/app/src/main/java/com/group5/controller/DetailActivity.java
@@ -205,9 +205,6 @@ public class DetailActivity extends AppCompatActivity  implements NavigationView
     @Override
     protected void onResume() {
         super.onResume();
-//        initViewPagerContentDetail();
-//        tabHost.setCurrentTab(0);
-//        viewPagerContentDetail.setCurrentItem(0);
         //Set Login to show Bookmark Button
         if(UserServices.getCurrentUser() == null) {
             fabBookmark.hide();
